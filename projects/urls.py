7c59a23d2fc9c724551b99701a6d7e13b2c18148
Italiz <italizv@gmail.com>
2023-03-06 20:48:28 -0500

    Feature 13 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from projects.views import show_project
+from projects.views import show_project, show_task
 
 urlpatterns = [
     path("", show_project, name="list_projects"),
+    path("<int:id>/", show_task, name="show_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from projects.models import Project
+from tasks.models import Task
 from django.contrib.auth.decorators import login_required
 
 
@@ -11,3 +12,14 @@ def show_project(request):
         "project_list": projects,
     }
     return render(request, "projects/list_project.html", context)
+
+
+@login_required(redirect_field_name="user_login")
+def show_task(request, id):
+    tasks = Task.objects.filter(project=id)
+    context = {
+        "task_list": tasks,
+        "project": Project.objects.get(id=id)
+    }
+
+    return render(request, "projects/detail_project.html", context)

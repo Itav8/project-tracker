1db82337c58380e64eaabf876500c6e6c4b2d785
Italiz <italizv@gmail.com>
2023-03-07 11:55:21 -0500

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
-from projects.views import show_project, show_task
+from projects.views import create_project, show_project, show_task
 
 urlpatterns = [
     path("", show_project, name="list_projects"),
     path("<int:id>/", show_task, name="show_project"),
+    path("create/", create_project, name="create_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from projects.forms import ProjectForm
 from projects.models import Project
 from tasks.models import Task
 from django.contrib.auth.decorators import login_required
@@ -17,9 +18,23 @@ def show_project(request):
 @login_required(redirect_field_name="user_login")
 def show_task(request, id):
     tasks = Task.objects.filter(project=id)
-    context = {
-        "task_list": tasks,
-        "project": Project.objects.get(id=id)
-    }
+    context = {"task_list": tasks, "project": Project.objects.get(id=id)}
 
     return render(request, "projects/detail_project.html", context)
+
+
+@login_required(redirect_field_name="user_login")
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.owner = request.user
+            project.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/create_project.html", context)
